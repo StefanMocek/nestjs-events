3c3eb46288df67fde32d6491d6cf3e865a8382dc
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-07 21:38:48 +0000

    getEventsBaseQuery method in events service

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,2 +1,16 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Event } from "./entity/event.entity";
+
 export class EventsService {
+    constructor(
+        @InjectRepository(Event)
+        private readonly eventsRepository: Repository<Event>
+    ) { }
+
+    private getEventsBaseQuery() {
+        return this.eventsRepository
+            .createQueryBuilder('e')
+            .orderBy('e.id', 'DESC')
+    }
 }
