d288c88fe375f0901cc03055a3db73d2f9788205
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-10 13:51:22 +0000

    auth service - generate jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) { }
+
     @Post('login')
     @UseGuards(AuthGuard('local'))
     async login(@Request() req) {
         return {
             userId: req.user.id,
-            token: 'token will go here'
+            token: this.authService.getTokenForUser(req.user)
         }
     }
 }
