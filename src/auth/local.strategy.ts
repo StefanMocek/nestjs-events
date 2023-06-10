60194772441941f3af066be37d22590bdf85d062
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-10 08:28:11 +0000

    temp local strategy

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { Repository } from "typeorm";
@@ -17,6 +17,21 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     public async validate(username: string, password: string): Promise<any> {
+        const user = await this.userRepository.findOne({
+            where: {userName: username}
+        })
 
+        if(!user) {
+            this.logger.debug(`User ${username} not found`);
+            throw new UnauthorizedException();
+        };
+
+        // temporary. Password stored in DB 
+        if(password !== user.password){
+            this.logger.debug('Wrong credentials');
+            throw new UnauthorizedException();   
+        };
+
+        return user;
     }
 }
