148638cb4bae1036155f91d13d009ba16ecaa5fe
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-12 19:46:11 +0000

    imported all of controllers

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -4,12 +4,20 @@ import { Event } from "./entity/event.entity";
 import { EventsController } from "./controllers/events.controller";
 import { EventsService } from "./services/events.service";
 import { AttendeesService } from "./services/attendees.service";
+import { EventsAttendeesController } from "./controllers/events-attendees.controller";
+import { EventsOrganizedByUserController } from "./controllers/events-organized-by-user.controller";
+import { CurrentUserEventAttendanceController } from "./controllers/current-user-event-attendance.controller";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Event]),
     ],
-    controllers: [EventsController],
+    controllers: [
+        EventsController,
+        EventsAttendeesController,
+        EventsOrganizedByUserController,
+        CurrentUserEventAttendanceController
+    ],
     providers: [EventsService, AttendeesService]
 })
 export class EventsModule { }
