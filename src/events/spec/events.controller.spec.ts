48182cb50a9ec073c5ff19ce264568d938086a06
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-14 15:34:30 +0000

    events.controller findAll test - mock the method

--- src/events/spec/events.controller.spec.ts
+++ src/events/spec/events.controller.spec.ts
@@ -2,6 +2,7 @@ import { Repository } from "typeorm";
 import { EventsController } from "../controllers/events.controller";
 import { EventsService } from "../services/events.service";
 import { Event } from "../entity/event.entity";
+import { ListEvents } from "../input/list-events";
 
 describe('EventsController tests', () => {
     let eventsController: EventsController;
@@ -13,4 +14,18 @@ describe('EventsController tests', () => {
         eventsController = new EventsController(eventsService)
     })
 
+    it('Should return list of events', async () => {
+        const result = {
+            first: 1,
+            last: 1,
+            limit: 10,
+            data: []
+        };
+
+        eventsService.getEventsWithAttendeeCountFilteredPaginated 
+            = jest.fn().mockImplementation((): any => result);
+        
+        expect(await eventsController.findAll(new ListEvents))
+            .toEqual(result)
+    })
 });
